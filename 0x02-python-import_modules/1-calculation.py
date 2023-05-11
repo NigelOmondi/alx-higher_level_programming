09309a220a092d9bbd6b3721c0b6bda4ba57599b
Nigel Omondi <nigelomondi62@gmail.com>
2023-05-11 23:23:57 +0300

    Do some math

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """"Print the addition, subtraction, multiplication 
-    and division of the numbers 5 and 10.
-    """
+   
     from calculator_1 import add, sub, mul, div
     a =10
     b = 5
