f49012efddb3e38fd2fd509bb5c2ea9ce3dd4d87
Nigel Omondi <nigelomondi62@gmail.com>
2023-05-15 23:56:14 +0300

    Print matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # iterate over each row in the matrix
-    for row in matrix:
-        # iterate over each integer in the row
-        for num in row:
-            # print the integer with a width of 5 (adjust as necessary)
-            # followed by a space to separate it from the next integer
-            print("{:5}".format(num), end="")
-        # print a newline after each row is complete
-        print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
