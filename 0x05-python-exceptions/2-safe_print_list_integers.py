a0a462fd261aaaf80b4fb50483d688c709356518
Nigel Omondi <nigelomondi62@gmail.com>
2023-05-31 16:45:03 +0300

    Print x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,12 +13,11 @@ def safe_print_list_integers(my_list=[], x=0):
         int: The real number of integers printed.
     """
     count = 0
-    try:
-        for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-    except IndexError:
-        pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
     print()
     return count
