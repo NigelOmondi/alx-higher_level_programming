1bf9eaa6febd87efeba863a64ff6c4b208da2708
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-06 20:29:26 +0300

    Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,9 +4,19 @@ that prevents the user from dynamically creating new instance attributes"""
 
 
 class LockedClass:
+    """
+    A class that prevents dynamic creation of new instance attributes,
+    except for the attribute 'first_name'.
+    """
     __slots__ = ['first_name']
 
     def __setattr__(self, name, value):
+        """
+        Intercept attribute assignments and raise an AttributeError
+        if the attribute being assigned is not 'first_name' or
+        already exists in the instance.
+        """
+
         if not hasattr(self, name) and name != 'first_name':
             raise AttributeError(
                 "'LockedClass' object has no attribute '{}'".format(name)
