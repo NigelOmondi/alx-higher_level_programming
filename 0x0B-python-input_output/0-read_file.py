6f81b65d237abcd7be2f8274592ff817e8158917
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-13 10:42:26 +0300

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a function to read file"""
+
+
+def read_file(filename=""):
+    """
+    Read a text file and print its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to an empty string.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
