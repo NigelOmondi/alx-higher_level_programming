2a4522c512fb1c65c5838233a6d781e6b3f441a2
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-13 20:49:53 +0300

    Return the dictionary description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,7 +6,5 @@ for JSON serialization of an object:
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object"""
+    """returns the dictionary description with simple data structure"""
     return obj.__dict__
