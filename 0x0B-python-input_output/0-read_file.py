5f0e43f7238f30906badf90ef38936693327cda6
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-13 20:18:52 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to standard output"""
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read(), end="")
