809af7d56ca09795f25bae559f77ca5706fe9c23
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-19 11:43:29 +0300

    Write the class Square that inherits from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define the class Square"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from the Rectangle class.
+
+    Attributes:
+        size (int): The size of the square.
+        x (int): The x-coordinate of the square's position.
+        y (int): The y-coordinate of the square's position.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
+            id (int, optional): The identifier for the object. Defaults to None.
+
+        Raises:
+            TypeError: If the size, x, or y arguments are not integers.
+            ValueError: If the size, x, or y is less than or equal to 0.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+
+        Returns:
+            str: The string representation of the square.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    # No need to redefine the area() and display() methods as they are inherited from the Rectangle class
