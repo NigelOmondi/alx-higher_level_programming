8eccbfac9e806853601f78315bb0b1b2f6c9edd3
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-19 12:30:52 +0300

    Add the static method def to_json_string(list_dictionaries)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Define class base"""
+
+
+import json
 
 
 class Base:
     """
-    Base class for managing the id attribute in all classes.
+    Base class for other classes in the project.
 
     Attributes:
-        __nb_objects (int): Private class attribute to keep track of the number of instances.
-        id (int): Public instance attribute representing the object's identifier.
+        __nb_objects (int): Counter for the number of objects.
     """
 
     __nb_objects = 0
@@ -18,14 +21,24 @@ class Base:
 
         Args:
             id (int, optional): The identifier for the object. Defaults to None.
-
-        Notes:
-            - If the id argument is provided, it will be assigned to the id attribute.
-            - If the id argument is not provided, __nb_objects will be incremented
-              and the new value will be assigned to the id attribute.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): The list of dictionaries to convert.
+
+        Returns:
+            str: The JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
