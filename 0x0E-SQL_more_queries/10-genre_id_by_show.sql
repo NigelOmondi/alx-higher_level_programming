e528efe8fd9a56126d1714904ecbdb179c2f298d
NigelOmondi <nigelomondi62@gmail.com>
2023-07-19 17:24:43 +0300

    Import the database dump from hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+--Import the database dump from hbtn_0d_tvshows
+--List all shows contained in the imported database
+--that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_shows_genres
+ON tv_shows.id=tv_show_genres.show_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
