43bb38dee9d72b9530a81aee51b091ed4543f17f
root <root@QuantumShift>
2023-08-18 23:17:14 +0300

    create the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
---Create database hbtn_0d_usa and table states in database
---hbtn_0d_usa.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.usa (
-		PRIMARY KEY(`id`),
-		`id` INT	NOT NULL AUTO_INCREMENT,
-		`name` VARCHAR(256) NOT NULL
+-- Create database hbtn_0d_usa and table states in database
+-- hbtn_0d_usa.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
