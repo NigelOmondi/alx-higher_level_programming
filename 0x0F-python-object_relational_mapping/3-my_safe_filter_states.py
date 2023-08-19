3458c6b350f71ccd3e04827abde716eff2d44721
root <root@QuantumShift>
2023-08-19 11:01:33 +0300

    write a script that is safe from MySQL injections!

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 '''
-A script that takes in arguments and displays all values 
-in the states table of hbtn_0e_0_usa where name matches 
-the argument.
+A script that takes in arguments 
+and displays all values 
+in the states table of hbtn_0e_0_usa 
+where name matches the argument.
 This script should be safe from MySQL injections!
 '''
 
