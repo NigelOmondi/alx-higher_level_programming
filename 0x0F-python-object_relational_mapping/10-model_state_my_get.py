e4f0ad30de10c1441807573170496ad635c1c918
root <root@QuantumShift>
2023-08-19 13:09:53 +0300

    Print the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,14 +5,16 @@ Write a script that prints the State object with
 the name passed as argument from the database hbtn_0e_6_usa
 
 Your script should take 4 arguments:
-mysql username, mysql password, database name and state name to search (SQL injection free)
+mysql username, mysql password, database name and
+state name to search (SQL injection free)
 
 You must use the module SQLAlchemy
 
 You must import State and Base from model_state 
 - from model_state import Base, State
 
-Your script should connect to a MySQL server running on localhost at port 3306
+Your script should connect to a MySQL server
+running on localhost at port 3306
 
 You can assume you have one record with the state name to search
 
