3f125b3b977d8101809153415ab18aa24e9c774b
NigelOmondi <nigelomondi62@gmail.com>
2023-08-31 22:54:48 +0300

    Send a request and displays the size of response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in URL, sends a request to the URL
-# and displays the size of the body of response.
+# Sends a request and displays the size of the response body.
 curl -s "$1" | wc -c
