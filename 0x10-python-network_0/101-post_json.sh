c9e8fd4d045f43d84ce9567baeaee44ed965d26b
NigelOmondi <nigelomondi62@gmail.com>
2023-08-31 23:25:12 +0300

    Send a JSON POST request and display body response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Send a JSON POST request and display
-# the body of the response.
+# Send a JSON POST request and display the body of the response.
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
