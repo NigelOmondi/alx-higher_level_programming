493643e04786ee86302fcb1b56ab7655f36f0048
NigelOmondi <nigelomondi62@gmail.com>
2023-08-31 23:17:22 +0300

    Get cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in URL, sends GET request to URL and
-# displays body of the response.
+# Sends GET request to URL and displays body of the response.
 curl -sH "X-School-User-Id: 98" "$1"
