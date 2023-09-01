09bbe3ad7316f404889387efacdf09f9b81e406e
NigelOmondi <nigelomondi62@gmail.com>
2023-09-01 21:53:28 +0300

    Send POST request with email as parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Takes URL and email, sends POST request and displays body of response"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Create a dictionary with email parameter
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    try:
+        with urllib.request.urlopen(url, data=data) as response:
+            body = response.read().decode('utf-8')
+            print("Your email is:", email)
+            print(body)
+    except urllib.error.URLError as e:
+        print(f"Error sending POST request: {e}")
