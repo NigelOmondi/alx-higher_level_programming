ff6acc075c6492a74481b4baac4ef18e7a4e6e05
NigelOmondi <nigelomondi62@gmail.com>
2023-09-01 22:10:50 +0300

    Manage urllib.error.HTTPError exceptions

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Manage HTTPError exception and print error code"""
+
+import sys
+import urrlib.request
+import urllib.error
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./3-error_code.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
