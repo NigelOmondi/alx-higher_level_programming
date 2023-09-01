98ff36f45783e95b44fc39e3c9487b4bb72ff46c
NigelOmondi <nigelomondi62@gmail.com>
2023-09-02 00:29:30 +0300

    Get error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     # Send a GET request to the URL
     response = requests.get(url)
-    # Display the body of response
-    print(response.text)
     # Check HTTP status code and print error message if > 400
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
