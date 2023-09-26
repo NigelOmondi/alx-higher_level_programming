41a608a20846dbe41c7c039563b812d49f16a236
NigelOmondi <nigelomondi62@gmail.com>
2023-09-26 09:21:27 +0300

    Write a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('The file has been written to and saved succesfully');
+  }
+});
